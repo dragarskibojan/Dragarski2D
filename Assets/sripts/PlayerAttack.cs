e0b296ab65f493c5821431893edf7bfbecccfc4d
dragarskibojan <dragarskibojan@gmail.com>
2026-07-16 19:49:32 +0200

    resavanje greski za ognenata topka

--- Assets/sripts/PlayerAttack.cs
+++ Assets/sripts/PlayerAttack.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCoolDown;
+    [SerializeField] private Transform firePoint; 
+    [SerializeField] private GameObject[] fireballs;
+
     private Animator anim;
-    private PlayerMovement PlayerMovement;
+    private PlayerMovement playerMovement; 
     private float cooldownTimer = Mathf.Infinity;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        PlayerMovement = GetComponent<PlayerMovement>();
+        playerMovement = GetComponent<PlayerMovement>(); 
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && cooldownTimer > attackCoolDown && PlayerMovement.canAttack())
+        if (Input.GetMouseButtonDown(0) && cooldownTimer > attackCoolDown && playerMovement.canAttack())
         {
             Attack();
         }
@@ -27,6 +30,20 @@ public class PlayerAttack : MonoBehaviour
     {
         anim.SetTrigger("attack");
         cooldownTimer = 0;
-        //pool fireballs
+
+        int id = FindFireball();
+
+        fireballs[id].transform.position = firePoint.position;
+        fireballs[id].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 }
--- Assets/sripts/Projectile.cs
+++ Assets/sripts/Projectile.cs
@@ -5,10 +5,9 @@ public class Projectile : MonoBehaviour
     [SerializeField] private float speed;
     private float direction;
     private bool hit;
-    private float lifetime;
 
-    private Animator anim;
     private BoxCollider2D boxCollider;
+    private Animator anim;
 
     private void Awake()
     {
@@ -19,28 +18,13 @@ public class Projectile : MonoBehaviour
     private void Update()
     {
         if (hit) return;
+
         float movementSpeed = speed * Time.deltaTime * direction;
         transform.Translate(movementSpeed, 0, 0);
-
-
-        lifetime += Time.deltaTime;
-        if (lifetime > 5) 
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        hit = true;
-        boxCollider.enabled = false;
-        
-        anim.SetTrigger("explode"); 
     }
 
     public void SetDirection(float _direction)
     {
-        lifetime = 0; 
         direction = _direction;
         gameObject.SetActive(true);
         hit = false;
@@ -53,6 +37,13 @@ public class Projectile : MonoBehaviour
         }
 
         transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
     }
 
     private void Deactivate()

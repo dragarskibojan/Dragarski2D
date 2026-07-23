bffba8cfa45714a26fb49c755221498c0a7c5a98
dragarskibojan <dragarskibojan@gmail.com>
2026-07-23 15:30:11 +0200

    IFrames

--- Assets/sripts/Health/Health.cs
+++ Assets/sripts/Health/Health.cs
@@ -1,30 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth = 3f;
 
     public float currentHealth { get; private set; }
 
     private Animator anim;
     private bool dead;
+    private bool isInvulnerable;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration; 
+    [SerializeField] private int numberOfFlashes;   
+    private SpriteRenderer spriteRend;
 
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
     {
-        if (dead) return; 
+        if (dead || isInvulnerable) return; 
 
         currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
 
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            // Invincibility frames (iframes)
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -48,5 +57,27 @@ public class Health : MonoBehaviour
         if (dead) return; 
 
         currentHealth = Mathf.Clamp(currentHealth + value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        isInvulnerable = true; 
+
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+
+        float flashDelay = iFramesDuration / (numberOfFlashes * 2);
+
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f); 
+            yield return new WaitForSeconds(flashDelay);
+            
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(flashDelay);
+        }
+
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        
+        isInvulnerable = false; 
     }
 }

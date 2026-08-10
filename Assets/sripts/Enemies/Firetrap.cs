797f47fe4e42a37e4c51aae74b3e160aa68e646c
dragarskibojan <dragarskibojan@gmail.com>
2026-08-10 16:28:01 +0200

    #9

--- Assets/sripts/Enemies/Firetrap.cs
+++ Assets/sripts/Enemies/Firetrap.cs
@@ -1,6 +1,55 @@
+using System.Collections;
 using UnityEngine;
 
 public class Firetrap : MonoBehaviour
 {
-    
+    [SerializeField] private float damage = 1f;
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay = 2f;
+    [SerializeField] private float activeTime = 2f;
+
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (!triggered)
+            {
+                StartCoroutine(ActivateFiretrap());
+            }
+
+            if (active)
+            {
+                Health playerHealth = collision.GetComponent<Health>();
+                if (playerHealth != null)
+                {
+                    playerHealth.TakeDamage(damage);
+                }
+            }
+        }
+    }
+
+    private IEnumerator ActivateFiretrap()
+    {
+        triggered = true;
+        spriteRend.color = Color.red; 
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white; 
+        active = true;
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+    }
 }
